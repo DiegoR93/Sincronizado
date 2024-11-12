56682460919d1377e944f9f7437be4750e09aeb4
DiegoUtalca <diego.rojas@utalca.cl>
2024-11-12 17:27:23 -0300

    Update navi.php

---
+++ navi.php
@@ -0,0 +1,8 @@
+<?php
+// Simulando un bloque de relleno con código repetitivo
+for ($i = 0; $i < 5; $i++) {
+    echo "Este es un mensaje de relleno: Iteración $i<br>";
+}
+
+// Fin del archivo de ejemplo
+?>
